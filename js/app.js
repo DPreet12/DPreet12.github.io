4b8cf6e2916f585eac0926f583899c0e22c394bd
DPreet12 <dharampreetsinghsandhu236@gmail.com>
2024-04-06 20:33:29 -0400

    feat:  porfolio website

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
 /*==================== toggle icon navbar ====================*/
 /*==================== scroll sections active link ====================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
 /*==================== sticky navbar ====================*/
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
 
+let header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 100);
+
+menuIcon.classList.remove("bx-x");
+menuIcon.classList.remove("active");
+
 /*==================== scroll reveal ====================*/
+
+ScrollReveal( {
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top"});
+ScrollReveal().reveal(".home-img, .services-container, .contact form", { origin: "bottom"});
+ScrollReveal().reveal(".home-content h1, .about-img", {origin: "left"});
+ScrollReveal().reveal(".home-content p, .about-content", {origin: "right"});
 /*==================== typed js ====================*/
 
-const headerNew = document.createElement("header");
+const typed = new Typed(".multiple-text", {
+    strings: ["Frontend Engineer", "Backend Engineer", "FullStack Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+/*const headerNew = document.createElement("header");
 headerNew.setAttribute("class", "header")
 const aNew = document.createElement("a");
 aNew.href = "#";
@@ -42,7 +74,7 @@ aContact.textContent = "Contact"
 //headerNew.append(aNew, iNew, navNew)
 //navNew.append( aHome, aAbout, aServices, aPortfolio, aContact)
 
-/*headerNew.append(aNew)
+headerNew.append(aNew)
 headerNew.append(iNew)
 headerNew.append(navNew)
 navNew.append(aHome);
@@ -130,3 +162,4 @@ selectBody.append(sectionNew)
 console.log(headerNew)
 console.log(sectionNew)
 */
+
